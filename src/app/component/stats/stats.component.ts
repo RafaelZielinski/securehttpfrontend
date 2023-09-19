93f6f2a693f14423b102dd4a5f791abf2f140f15
RafaelZielinski <rafekzielinski@wp.pl>
2023-09-19 20:11:38 +0200

    adding stats component

--- src/app/component/stats/stats.component.ts
+++ src/app/component/stats/stats.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-stats',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./stats.component.css']
 })
 export class StatsComponent {
-
+  // @Input() stats: Stats;
 }
