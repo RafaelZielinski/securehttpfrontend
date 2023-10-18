785f920e1475bb1965df066e217d9a5a4a11f5b8
RafaelZielinski <rafekzielinski@wp.pl>
2023-10-18 22:07:26 +0200

    adding missing interfaces

---
+++ src/app/service/customer.service.ts
@@ -0,0 +1,44 @@
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { Observable, catchError, tap, throwError, pipe } from 'rxjs';
+import { CustomHttpResponse, Page, Profile } from '../interface/appstates';
+import { User } from "../interface/user";
+import { Key } from "../enum/key.enum";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CustomerService {
+
+  private readonly server: string = 'http://localhost:8095';
+
+  constructor(private http: HttpClient,) {
+  }
+
+  customers$ = (page: number = 0) => <Observable<CustomHttpResponse<Page & User>>>
+    this.http.get<CustomHttpResponse<Profile>>
+      (`${this.server}/customer/list?page=${page}`)
+      .pipe(
+        tap(console.log),
+        catchError(this.handleError)
+      );
+
+  private handleError(error: HttpErrorResponse): Observable<never> {
+    console.log(error);
+    let errorMessage: string;
+    if (error.error instanceof ErrorEvent) {
+      errorMessage = `A client error occured - ${error.error.message}`
+      console.log(errorMessage)
+    } else {
+      if (error.error.reason) {
+        errorMessage = error.error.reason;
+        console.log(errorMessage);
+      } else {
+        errorMessage = `An error occured - Error codeStatus ${error.status}`;
+        console.log(errorMessage)
+      }
+    }
+    return throwError(() => errorMessage);
+  }
+}
