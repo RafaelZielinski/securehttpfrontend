1964a18173180d4fd39985299d716bc19320d2c6
RafaelZielinski <rafekzielinski@wp.pl>
2023-10-30 19:54:37 +0100

    adding showing your invoices

--- src/app/component/invoices/invoices.component.ts
+++ src/app/component/invoices/invoices.component.ts
@@ -1,4 +1,14 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
+import { BehaviorSubject, Observable, catchError, map, of, startWith, switchMap } from 'rxjs';
+import { DataState } from 'src/app/enum/datastate.enum';
+import { EventType } from 'src/app/enum/event-type.enum';
+import { CustomHttpResponse, Page} from 'src/app/interface/appstates';
+import { Invoice } from 'src/app/interface/invoice';
+import { State } from 'src/app/interface/state';
+import { User } from 'src/app/interface/user';
+import { CustomerService } from 'src/app/service/customer.service';
+import { UserService } from 'src/app/service/user.service';
 
 @Component({
   selector: 'app-invoices',
@@ -7,4 +17,52 @@ import { Component } from '@angular/core';
 })
 export class InvoicesComponent {
 
+  invoicesState$: Observable<State<CustomHttpResponse<Page<Invoice> & User>>>;
+  private dataSubject = new BehaviorSubject<CustomHttpResponse<Page<Invoice> & User>>(null);
+  private isLoadingubject = new BehaviorSubject<boolean>(false);
+  isLoading$ = this.isLoadingubject.asObservable();
+  private currentPageSubject = new BehaviorSubject<number>(0);
+  currentPage$ = this.currentPageSubject.asObservable();
+  private showLogsSubject = new BehaviorSubject<boolean>(false);
+  showLogs$ = this.showLogsSubject.asObservable();
+  readonly DataState = DataState;
+  readonly EventType = EventType;
+
+  constructor(private router: Router, private userService: UserService, private customerService: CustomerService) {
+  }
+
+  ngOnInit(): void {
+    this.invoicesState$ = this.customerService.
+      invoices$()
+      .pipe(
+        map(response => {
+          console.log(response);
+          this.dataSubject.next(response);
+          return { dataState: DataState.LOADED, appData: response };
+        }),
+        startWith({ dataState: DataState.LOADING }),
+        catchError((error: string) => {
+          return of({ dataState: DataState.ERROR, error })
+        })
+      )
+  }
+
+  goToPage(pageNumber?: number): void {
+    this.invoicesState$ = this.customerService.invoices$(pageNumber)
+      .pipe(
+        map(response => {
+          console.log(response);
+          this.dataSubject.next(response);
+          this.currentPageSubject.next(pageNumber);
+          return { dataState: DataState.LOADED, appData: response };
+        }),
+        startWith({ dataState: DataState.LOADED, appData: this.dataSubject.value }),
+        catchError((error: string) => {
+          return of({ dataState: DataState.ERROR, error, appData: this.dataSubject.value })
+        })
+      )
+  }
+  goToNextOrPreviosPage(direction?: string): void {
+    this.goToPage(direction === 'forward' ? this.currentPageSubject.value + 1 : this.currentPageSubject.value - 1);
+  }
 }
